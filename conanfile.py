7349f6a37e6e2bafc97c8671899431eab31b4469
RobertHue <robert.huem@gmail.com>
2023-08-03 16:00:26 +0200

    fixed qt::with_icu problem because its not available on Linux without namespace

--- conanfile.py
+++ conanfile.py
@@ -3,6 +3,7 @@ from conan import ConanFile
 from conan.tools.cmake import cmake_layout
 from conan.tools.env import Environment
 
+
 class MCTS_4Wins(ConanFile):
     name = "mypkg"
     version = "1.0"
@@ -10,14 +11,15 @@ class MCTS_4Wins(ConanFile):
     generators = "CMakeDeps", "CMakeToolchain"
 
     def requirements(self):
-        self.requires("boost/[>=1.0.0]")
-        self.requires("qt/5.15.9")
+        self.requires("boost/[>=1.82.0]")
+        self.requires("qt/[>=6.5.0]")
 
     def configure(self):
         self.options["boost"].shared = True
         self.options["qt"].shared = True
         self.options["qt"].opengl = "desktop"
         self.options["qt"].GUI = True
+        self.options["qt"].with_icu = False  # because its not available on Linux without namespace
 
     def build_requirements(self):
         self.tool_requires("cmake/3.26.4")
